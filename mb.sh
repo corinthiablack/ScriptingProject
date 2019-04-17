edaf5b662dfd4165ecab018fbd6391023697b2b4
Miles <loganmh94@yahoo.com>
2019-04-17 17:15:25 -0500

    added script to run MrBayes

---
+++ mb.sh
@@ -0,0 +1,11 @@
+#!/bin/sh
+#
+# Creates new directory and changes to it 
+mkdir mb_output
+cd mb_output
+# load the MrBayes environment
+source /opt/asn/etc/asn-bash-profiles-special/modules.sh
+module load mrbayes
+# Run my job
+mb ../catfishes_practice.nex >log.txt
+
